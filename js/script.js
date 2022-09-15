7b312d89d4ba0383e18b42bdf5072a5c61115c14
Larissa <98964013+larissa4p@users.noreply.github.com>
2022-09-15 11:57:22 -0300

    input, search

--- js/script.js
+++ js/script.js
@@ -2,9 +2,12 @@ const pokemonName = document.querySelector(".pokemon__name");
 const pokemonNumber = document.querySelector(".pokemon__number");
 const pokemonImage = document.querySelector(".pokemon__image");
 
+const form = document.querySelector(".form");
+const input = document.querySelector(".input__search");
+
 const fetchPokemon = async (pokemon) => {
   const APIResponse = await fetch(
-    `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+    `https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`
   );
 
   const data = await APIResponse.json();
@@ -17,7 +20,17 @@ const renderPokemon = async (pokemon) => {
 
   pokemonName.innerHTML = data.name;
   pokemonNumber.innerHTML = data.id;
-  pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+  pokemonImage.src =
+    data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
+      "front_default"
+    ];
 };
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  renderPokemon(input.value);
+  input.value = '';
+});
 
 renderPokemon("25");
